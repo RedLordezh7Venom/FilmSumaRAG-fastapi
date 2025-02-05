8a9782346a497d09c2b69bb4b314a97f108857b6
lordpotato <54540149+RedLordezh7Venom@users.noreply.github.com>
2025-02-05 21:53:08 +0530

    finally working in production, set the CORS allowed sites for production to /summary

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ safety_settings = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://film-suma-rag-frontend-inky.vercel.app/","https://filmsumarag-frontend.onrender.com/","https://filmsumarag-frontend.onrender.com/summary/"],
+    allow_origins=["https://film-suma-rag-frontend-inky.vercel.app/","https://filmsumarag-frontend.onrender.com/","https://filmsumarag-frontend.onrender.com/summary/","https://film-suma-rag-frontend-inky.vercel.app/summary/"],
     allow_credentials=True,
     allow_methods=["*"],  # Restrict to needed methods
     allow_headers=["*"]
